cf5cc2acc02c8287cf8fe908c36d4a9465e7a562
Eduard Wille <eduardwille1@gmail.com>
2024-06-08 20:52:11 +0200

    Foundation created, type and inputs for answers
    I created the basic foundation for the program. A simple updating title with what type you're guessing for.
    
    Then the program creates inputs based on how many weaknesses the current type has.
    
    The user can enter by typing, when a correct answer is given the input will disable and turn green, if answer is incorrect it will clear the input so the user can retry.
    
    Upon filling in all answers correctly the program will reset and rerandomize.

---
+++ script.js
@@ -0,0 +1,79 @@
+var pokemonTypes = [
+    {type: 'fire', color: 'orange', weakness: ['water', 'ground', 'rock']},
+    {type: 'water', color: 'blue', weakness: ['grass', 'electric']},
+    {type: 'grass', color: 'green', weakness: ['fire', 'ice', 'poison', 'flying', 'bug']}
+];
+
+/** get container divs from html */
+var container = document.getElementById('container');
+var answerField = document.getElementById('answerField');
+var type = document.getElementById('type');
+
+/** create a temporary array
+ * this will be used to check and remove correct answers so no duplicate answers
+ */
+var tempWeaknessArray = [];
+
+/** starts first game on opening the window */
+window.onload = startGame();
+
+function startGame() {
+    /** generate random number based on how many pokemon types */
+    var randomizedNumber = Math.floor(Math.random() * pokemonTypes.length);
+    
+    /** push the weakness of current type into array per item so there is no link to array pokemonTypes */
+    for(b=0; b < pokemonTypes[randomizedNumber].weakness.length; b++){
+        tempWeaknessArray.push(pokemonTypes[randomizedNumber].weakness[b]);
+    }
+
+    /** put randomized pokemontype into the title and change color to match*/
+    type.innerHTML = pokemonTypes[randomizedNumber].type;
+    type.style.backgroundColor = pokemonTypes[randomizedNumber].color;
+
+    /** for loop that goes through all the weaknesses of the random type 
+     * creates input element per weakness
+     * input gets event on enter key which runs the checkAnswer function
+    */
+    for (i=0; i < pokemonTypes[randomizedNumber].weakness.length; i++) {
+        var input = document.createElement('input');
+
+        answerField.appendChild(input);
+        input.addEventListener("keydown", function (e) {
+            if (e.key === 'Enter') {
+                checkAnswer(this, this.value);
+            }
+        });
+    }
+}
+
+/** function that checks answer of a specific input
+ * then disables that input, and updates css a little
+ */
+function checkAnswer(input, answer) {
+    for(a=0; a < tempWeaknessArray.length; a++){
+        if(tempWeaknessArray[a] == answer){
+            input.disabled = 'true';
+            input.style.backgroundColor = 'green';
+            input.style.color = 'white';
+            tempWeaknessArray.splice(a, 1);
+        }
+    }
+
+    /** if answer is incorrect remove text so user can type right away */
+    if(input.style.backgroundColor != 'green'){
+        input.value = '';
+    }
+
+    /** if temporary array is empty all answers were given and reset game */
+    if(tempWeaknessArray == 0){
+        reset();
+    }
+}
+
+/** reset function, clears title, answers and starts a new game */
+function reset(){
+    tempWeaknessArray = [];
+    answerField.innerHTML = '';
+    type.innerHTML = '';
+    startGame();
+}
