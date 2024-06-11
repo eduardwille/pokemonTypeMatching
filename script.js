8b71d6b12490340e9389bdad655519eb9778d30e
Eduard Wille <eduardwille1@gmail.com>
2024-06-11 21:06:14 +0200

    Added a home menu + quit game function
    Added a incredibly basic home menu where you can choose what to play, right now there is only one game however.
    
    Also added a quit game function. that resets the game and goes back to the home menu.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /** get container divs from html */
-var container = document.getElementById('container');
+var homeMenu = document.getElementById('homeMenu');
+var container = document.getElementById('weaknessContainer');
 var answerField = document.getElementById('answerField');
 var type = document.getElementById('type');
 
@@ -8,9 +9,14 @@ var type = document.getElementById('type');
  */
 var tempWeaknessArray = [];
 
-/** starts first game on opening the window */
-window.onload = startGame();
+/** starts the weakness guessing game and hides the home menu */
+function startWeaknessGame() {
+    startGame();
+    container.style.display = 'block';
+    homeMenu.style.display = 'none';
+}
 
+/** large function that sets up everything needed for the game */
 function startGame() {
     /** generate random number based on how many pokemon types */
     var randomizedNumber = Math.floor(Math.random() * pokemonTypes.length);
@@ -99,4 +105,20 @@ function reset(){
     answerField.innerHTML = '';
     type.innerHTML = '';
     startGame();
+}
+
+/** function that quits the game and goes back to the home menu
+ * The confirm function runs a yes or no dialog for the user
+ * Empty all the gameplay variables so it'll be ready for the next game
+ * then hide the game window and show the home menu again
+ */
+function quitGame() {
+    if(confirm("Are you sure you want to quit?")){
+        tempWeaknessArray = [];
+        answerField.innerHTML = '';
+        type.innerHTML = '';
+    
+        container.style.display = 'none';
+        homeMenu.style.display = 'block';
+    }
 }
